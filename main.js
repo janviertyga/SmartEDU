34a3364c22e0c3f26eb32aba012322a640cd2947
Janviertyga <janviertyga@gmail.com>
2023-11-29 16:40:35 +0200

    Education

---
+++ main.js
@@ -0,0 +1,11 @@
+ // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+
+    
+
